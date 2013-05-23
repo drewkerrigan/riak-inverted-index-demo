bac9df027f29603e7048f63d4da8584ec8dc06c1
Dan Kerrigan <dankerrigan@basho.com>
2013-05-23 14:25:35 -0400

    Removed city/state index

--- load_data.rb
+++ load_data.rb
@@ -14,14 +14,12 @@ def load_data(filename)
 
       zombie.add_index('zip_bin', zombie.data[:zip])
       zombie.add_index('zip_inv', zombie.data[:zip])
-      zombie.add_index('city_inv', zombie.citystate)
       zombie.add_index('geohash_inv', zombie.geohash(4))
       zombie.save
 
       # Sibling resolution takes places when index is retrieved. Do periodically to avoid sibling explosion
       if i % 20 == 0
         zombie.search_index('zip_inv', zombie.data[:zip])
-        zombie.search_index('city_inv', zombie.citystate)
         zombie.search_index('geohash_inv', zombie.geohash(4))
       end
 
