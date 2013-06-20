4416738176ab22605c2854c0e59454e217c90a24
drewkerrigan <dkerrigan@basho.com>
2013-06-20 15:25:48 -0400

    load script for large files

---
+++ auto_load.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+proc_count=4
+data_file=$1
+script_dir=$(pwd)
+split_prefix=data_split
+
+line_count=$(wc -l $data_file | awk '{ print $1 }')
+split_count=$(((line_count/proc_count)+1))
+
+split -l $split_count $data_file $split_prefix
+
+for f in ${split_prefix}*
+do
+  cd $script_dir
+  ruby load_data.rb $f &
+  cd -
+done
