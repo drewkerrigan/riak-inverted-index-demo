5b3b91127b65b4ef680f1207ce56de03c14864a2
drewkerrigan <dkerrigan@basho.com>
2013-06-25 15:50:57 -0400

    modifying autoload

--- auto_load.sh
+++ auto_load.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-proc_count=4
+proc_count=$(grep processor /proc/cpuinfo|wc -l)
 data_file=$1
-script_dir=$(pwd)
 split_prefix=data_split
 
 line_count=$(wc -l $data_file | awk '{ print $1 }')
@@ -12,9 +11,9 @@ split -l $split_count $data_file $split_prefix
 
 for f in ${split_prefix}*
 do
-  cd $script_dir
   nohup ruby load_data.rb $f &
-  cd -
 done
+
+#ruby load_data.rb $1
 
 touch dataloaded.txt
