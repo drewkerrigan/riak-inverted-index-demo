0511c297b338ca6488bb6bc9926cf385de32e149
Dan Kerrigan <dankerrigan@basho.com>
2013-06-27 11:52:36 -0400

    rec/sec

--- load_data.rb
+++ load_data.rb
@@ -4,11 +4,17 @@ require('riak_crdts')
 require('./riak_hosts')
 require('./models/zombie')
 
+def time
+  Time.now.to_f * 1000.0
+end
+
 def load_data(filename)
 
   #client = Riak::Client.new(:protocol => 'pbc')
   client = RiakHosts.new.get_riak_connection
   zip3 = RiakCrdts::InvertedIndex.new(client, 'zip3_inv')
+
+  start = time
 
   File.open(filename) do |file|
 
@@ -25,6 +31,12 @@ def load_data(filename)
       zombie.add_index('zip_inv', zip)
       zombie.add_index('geohash_inv', zombie.geohash(4))
       zombie.save
+
+      if i % 50 == 0
+        total_time = (time - start)/1000
+        ops_sec = i / total_time
+        print "\r#{ops_sec} rec/sec                             "
+      end
 
     end
   end
